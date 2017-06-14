bb630612a8b220b3ad04c30c2c11e6592fb7f7d9
Igor Alexeenko <alexeenko.igor@gmail.com>
2017-06-15 00:43:33 +0300

    Производительность: разгрузка процессора

---
+++ cpu/example-01-dom-big/main.js
@@ -0,0 +1,74 @@
+'use strict';
+
+(function() {
+  const PICS_NUMBER = 381;
+  const PAGE_SIZE = 5000;
+
+  const container = document.querySelector('.layout');
+  const templateElement = document
+    .querySelector('#tmpl')
+    .content
+    .querySelector('.pic');
+
+
+  function* getPictureSwitcher(picsNumber) {
+    let currentPic = 0;
+    while (true) {
+      yield `pics/images${currentPic === 0 ? `` : `(${currentPic})`}`;
+      currentPic = currentPic === picsNumber ? 0 : currentPic + 1;
+    }
+  }
+
+
+  function* getPageSwitcher(pictureSwitcher) {
+    while (true) {
+      yield function* (pageSize) {
+        let i = pageSize;
+        while (i--) {
+          const el = templateElement.cloneNode(true);
+          el.querySelector('.pic-thumb').src = pictureSwitcher.next().value;
+          el.querySelector('.pic-like-counter').textContent = Math.floor(
+              Math.random() * 90) + 10;
+
+          yield el;
+        }
+      };
+    }
+  }
+
+
+  const switchPage = (pageSwitcher, pageSize) => {
+    container.innerHTML = '';
+
+    const fragment = document.createDocumentFragment();
+    for (const el of pageSwitcher.next().value(pageSize)) {
+      el.addEventListener('keydown', evt => {
+        evt.stopPropagation();
+        console.log(el, Math.random());
+      });
+
+      fragment.appendChild(el);
+    }
+
+    container.appendChild(fragment);
+  };
+
+
+  const initialize = (pageSize) => {
+    const SCROLL_GAP = 100;
+    const THROTTLE_INTERVAL = 100;
+    const pictureSwitcher = getPictureSwitcher(PICS_NUMBER);
+    const pageSwitcher = getPageSwitcher(pictureSwitcher);
+
+    [...document.querySelectorAll('.nav-item')].forEach((linkEl, i) => {
+      linkEl.onclick = (evt) => {
+        evt.preventDefault();
+        switchPage(pageSwitcher, PAGE_SIZE);
+      };
+    });
+
+    switchPage(pageSwitcher, PAGE_SIZE);
+  };
+
+  initialize(PAGE_SIZE);
+})();
